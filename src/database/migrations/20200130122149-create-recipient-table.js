56aee5991cac31b1b97d9df1825218c79ed05eea
Lucas Adorno <lucasadorno16@hotmail.com>
2020-01-30 21:54:09 -0300

    Correção do nome da tabela recipient para recipients

--- src/database/migrations/20200130122149-create-recipient-table.js
+++ src/database/migrations/20200130122149-create-recipient-table.js
@@ -1,6 +1,8 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('recipient', {
+    return queryInterface.createTable('recipients', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -47,6 +49,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('recipient');
+    return queryInterface.dropTable('recipients');
   },
 };
